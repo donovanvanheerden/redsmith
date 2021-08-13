2a291fccfe0f49051f2ee939c0fa8681682a5556
Donovan van Heerden <dp.vanheerden@gmail.com>
2021-08-13 11:14:51 +0200

    added typescript and moved files to src

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 
 const createWindow = () => {
   const win = new BrowserWindow({
