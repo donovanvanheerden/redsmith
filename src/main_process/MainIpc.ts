e95fe67f27c3461edc0928f02d238414e2f5fe73
Donovan van Heerden <dp.vanheerden@gmail.com>
2021-08-23 15:27:50 +0200

    added ioredis and basic redis commands for testing

--- src/main_process/MainIpc.ts
+++ src/main_process/MainIpc.ts
@@ -1,4 +1,11 @@
+import Redis from 'ioredis';
 import { ipcMain } from 'electron';
+
+const redis = new Redis({
+  host: '127.0.0.1',
+  port: 6379,
+  password: 'agvlive.redis',
+});
 
 ipcMain.on('async-message', (event, arg) => {
   console.log('async-message: ', arg); // prints "ping"
@@ -8,4 +15,24 @@ ipcMain.on('async-message', (event, arg) => {
 ipcMain.on('sync-message', (event, arg) => {
   console.log('sync-message: ', arg); // prints "ping"
   event.returnValue = 'pong';
+});
+
+ipcMain.on('get', async (event, key: string) => {
+  try {
+    const value = await redis.get(key);
+    event.reply('get-reply', value);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+ipcMain.on('keys', async (event) => {
+  try {
+    redis.select(1);
+
+    const keys = await redis.keys('*');
+    event.reply('keys-reply', keys);
+  } catch (error) {
+    console.log(error);
+  }
 });
