40d97842712d0be778d86300b0c108be01ae552f
Donovan van Heerden <dp.vanheerden@gmail.com>
2021-09-12 21:38:04 +0200

    clean up, removed unused functions and only allow async comms on IPC

--- src/preload.ts
+++ src/preload.ts
@@ -1,16 +1,11 @@
 import { ipcRenderer, contextBridge } from 'electron';
 import * as Messages from './core/WindowMessages';
 
-interface IpcWeb {
-  send: (channel: string, ...args: unknown[]) => unknown;
+export interface IpcWeb {
   sendAsync: <T>(channel: string, ...args: unknown[]) => Promise<T>;
-  receive: (channel: string, handler: (...args: unknown[]) => void) => void;
 }
 
 contextBridge.exposeInMainWorld('ipc', {
-  send: (channel, ...args) => {
-    return ipcRenderer.sendSync(channel, ...args);
-  },
   sendAsync: async (channel, ...args) => {
     return await new Promise((resolve, reject) => {
       if (channel !== Messages.CHANNEL_NAME)
@@ -37,14 +32,6 @@ contextBridge.exposeInMainWorld('ipc', {
 
         ipcRenderer.removeAllListeners(channel);
       });
-    });
-  },
-  receive: (channel, handler) => {
-    ipcRenderer.on(channel, (_, ...args) => {
-      handler(...args);
-
-      // remove listeners after event is handled?
-      ipcRenderer.removeAllListeners(channel);
     });
   },
 } as IpcWeb);
