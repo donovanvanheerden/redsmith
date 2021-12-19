64fdb9b02f12656993e56e9b90f5620e19c78ba3
Donovan van Heerden <dp.vanheerden@gmail.com>
2021-12-19 23:27:31 +0200

    updated imports to align with new components

--- src/render_process/components/layout/layout.tsx
+++ src/render_process/components/layout/layout.tsx
@@ -2,19 +2,21 @@ import { Grid } from '@mui/material';
 import React from 'react';
 
 import { KeyList } from '../keyList';
-import { SideNav } from '../sideNav';
+
 import { ValueDetail } from '../valueDetail';
 
 import { Root } from './layout.styles';
 import { ConnectionForm } from '../connectionform';
-import { ConnectionSwitcher } from '../connectionSwitcher';
+
+import { ConnectionBar } from '../connectionBar';
+import { DbSelector } from '../dbSelector';
 
 const Layout = (): JSX.Element => (
   <React.Fragment>
     <ConnectionForm />
     <Root>
-      <ConnectionSwitcher />
-      <SideNav />
+      <ConnectionBar />
+      <DbSelector />
       <Grid container>
         <KeyList />
         <ValueDetail />
