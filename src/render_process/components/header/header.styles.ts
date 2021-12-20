ac4a2c66a208fa0f6eec1929fbe2219bada42761
Donovan van Heerden <dp.vanheerden@gmail.com>
2021-12-20 15:09:33 +0200

    switch to h5 and updated styling

--- src/render_process/components/header/header.styles.ts
+++ src/render_process/components/header/header.styles.ts
@@ -3,4 +3,7 @@ import { styled } from '@mui/material/styles';
 
 export const HeadingTitle = styled(Typography)(({ theme }) => ({
   padding: theme.spacing(3),
+  color: theme.palette.common.white,
+  fontWeight: 500,
+  textTransform: 'uppercase',
 }));
